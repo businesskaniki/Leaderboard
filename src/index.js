ed2e2650d4a6a770307119c5ea583968340b4223
businesskaniki <businesskanikii@gmail.com>
2022-07-27 17:57:00 +0300

    fixing linter errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import './style.css';
 import newgame from './newgame.js';
-import saveuserAndScores from "./save.js";
+import saveuserAndScores from './save.js';
 import getdata from './getdata.js';
 const submit = document.getElementById('submit');
 const name = document.getElementById('name');
 const score = document.getElementById('score');
 
-
 document.addEventListener('DOMContentLoaded', function () {
-    getdata()
-  }); 
+  getdata();
+});
 
-submit.addEventListener('click',(event) =>{
-    event.preventDefault();
-    saveuserAndScores();
-    if (name.value == '' | score.value == '') {
-        alert('please use letters for name');
-      } else {
-        name.value = '';
-        score.value = '';
-      } 
-})
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  saveuserAndScores();
+  if ((name.value == '') | (score.value == '')) {
+    alert('please use letters for name');
+  } else {
+    name.value = '';
+    score.value = '';
+  }
+});
 
-newgame();  
+newgame();
